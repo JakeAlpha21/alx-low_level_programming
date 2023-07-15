f0c586ea229c28292bc2cc4550dbd84cd62a4a33
Oluwaloba Jacob <obajacob21@gmail.com>
2023-07-14 21:07:59 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Beginning of prog
+ * Return: 0 (good looks)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
