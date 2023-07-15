efe414492f4d6c5ae53a29708808b4cae9ebfb1b
Oluwaloba Jacob <obajacob21@gmail.com>
2023-07-14 21:01:16 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Beginning of prog
+ * Return: 0 (good looks)
+ */
+
+int main(void)
+{
+	char w;
+
+	w = 'a';
+	while
+		(w <= 'z') {
+			if ((w != 'q' && w != 'c') && w <= 'z')
+				putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
