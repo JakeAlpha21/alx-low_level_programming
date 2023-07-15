61dddf823f59645234a9b7a9a0766dfe6b2ea53d
Oluwaloba Jacob <obajacob21@gmail.com>
2023-07-14 21:17:58 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Beginning of prog
+ * Return: 0 (good looks)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
