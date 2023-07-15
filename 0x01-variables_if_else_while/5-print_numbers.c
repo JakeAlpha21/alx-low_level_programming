978ffa099a372cf6e2000b5acf0ef5a6270c25da
Oluwaloba Jacob <obajacob21@gmail.com>
2023-07-14 20:49:48 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Beginning of prog
+ * Return: 0 (good looks)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
