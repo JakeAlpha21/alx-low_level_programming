fb0b6e6b02324c2e081cb24a510affb64482d719
Oluwaloba Jacob <obajacob21@gmail.com>
2023-07-14 22:15:04 -0700

    Commit both please

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Beginning of code
+ * Return: 0(good looks)
+ */
+int main(void)
+{
+	char t;
+
+	t = 'a';
+	while
+		(t <= 'z') {
+			putchar(t);
+			t++;
+		}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Beginning of prog
+ * Return: 0 (looks good)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
