b39fe5eb719b5226227d338c81906aa83cb111e1
Oluwaloba Jacob <obajacob21@gmail.com>
2023-07-18 22:22:23 -0700

    files 8-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adding two values
+ * @t1: The first number that is calculated
+ * @t2: The second number that is calculated
+ * Return: An integer value
+ */
+
+int add(int t1, int t2)
+{
+	return (t1 + t2);
+}
